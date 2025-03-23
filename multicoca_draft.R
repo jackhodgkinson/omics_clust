c81d86f36f0eaa7f5fe90bec058ae289dcec667e
Jack Hodgkinson <jnhodgkinson2010@hotmail.co.uk>
2025-03-23 22:24:11 +0000

    Changed to function.

--- multicoca_draft.R
+++ multicoca_draft.R
@@ -2,16 +2,22 @@
 # Load packages 
 library(mclust)
 
+# MultiCOCA function 
+multicoca <- function(data) {
+
 # Initialize a list for GMM results
-mclust1 <- vector("list", ncol(sim_data))
+mclust1 <- vector("list", ncol(data))
 
 # Create an empty data frame with the same dimensions as sim_data
-sim_data2 <- as.data.frame(matrix(NA, nrow = nrow(sim_data), ncol = ncol(sim_data)))
-colnames(sim_data2) <- colnames(sim_data)  # Preserve column names
+data2 <- as.data.frame(matrix(NA, nrow = nrow(data), ncol = ncol(data)))
+colnames(data2) <- colnames(data)  # Preserve column names
 
 # Loop through each protein (column) and fit Mclust
-for (i in 1:ncol(sim_data)) {  
-  mclust1[[i]] <- Mclust(sim_data[, i])
-  sim_data2[, i] <- mclust1[[i]]$data  # Store clustered data column-wise
+for (i in 1:ncol(data)) {  
+  mclust1[[i]] <- Mclust(data[, i])
+  data2[, i] <- mclust1[[i]]$data  # Store clustered data column-wise
 }
+
+}
+
 
