eb6cec2073ad81f0049affdf8bf2203ef45a132f
Jack Hodgkinson <jnhodgkinson2010@hotmail.co.uk>
2025-03-24 13:48:39 +0000

    draft function for COCA

--- multicoca_draft.R
+++ multicoca_draft.R
@@ -1,23 +1,36 @@
 ## MultiCOCA
 # Load packages 
 library(mclust)
+library(coca)
 
 # MultiCOCA function 
-multicoca <- function(data) {
+multicoca <- function(data, clusters) {
 
 # Initialize a list for GMM results
 mclust1 <- vector("list", ncol(data))
 
 # Create an empty data frame with the same dimensions as sim_data
 data2 <- as.data.frame(matrix(NA, nrow = nrow(data), ncol = ncol(data)))
-colnames(data2) <- colnames(data)  # Preserve column names
 
 # Loop through each protein (column) and fit Mclust
 for (i in 1:ncol(data)) {  
   mclust1[[i]] <- Mclust(data[, i])
-  data2[, i] <- mclust1[[i]]$data  # Store clustered data column-wise
+  data2[, i] <- mclust1[[i]]$classification  # Store clustered data column-wise
 }
 
+# How do we deal if Mclust comes up with different numbers of clusters? 
+
+# Change data to MOC
+#moc <- coca::buildMOC(data2, M = ncol(data2), K = clusters)
+
+# Try to create MOC using function and then try to make MOC myself to avoid double clustering.
+# Visualise the MOC
+# 
+
+return(data2)
 }
+
+data2 <- multicoca(sim_data, 2)
+
 
 
