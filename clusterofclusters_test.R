e24100c6829fddf983699dc44fa8eda74dbdb0cb
Jack Hodgkinson <jnhodgkinson2010@hotmail.co.uk>
2025-04-04 11:39:10 +0100

    update to simulate both single and multiple clustering structures.

--- clusterofclusters_test.R
+++ clusterofclusters_test.R
@@ -6,18 +6,28 @@ source("clusterofclusters.R")
 # Load packages 
 library(mclust)
 
-# Create data
+# Create data - single clustering structure 
 N_col <- 10
 params1 <- list(
   cluster1 = list(mean = runif(N_col, -20, -10), sd = runif(N_col, 0.5, 1)),  
   cluster2 = list(mean = runif(N_col, 10, 20), sd = runif(N_col, 1, 5)), 
   cluster3 = list(mean = runif(N_col, 0, 0.5), sd = runif(N_col, 1, 10)))  
 sim_data <- simulateGMM(3, 1, params1, n_indiv = 419, n_col = N_col, random_seed = 4881, grouping = "random")
-clusters <- sim_data$cluster
+clusters1 <- sim_data$cluster
 sim_data <- sim_data[, 1:N_col]
 
-# Test on simulated data 
+# Test on data1
 outputCOCA <- clusterofclusters(sim_data)
 clusters2 <- outputCOCA$clusterLabels
-adjustedRandIndex(clusters, clusters2)
+adjustedRandIndex(clusters1, clusters2)
+
+# Create data - multiple clustering structures
+sim_data2 <- simulateGMM(3, 2, params1, n_indiv = 419, n_col = N_col, random_seed = 4881, grouping = "random")
+clusters1a <- sim_data2$cluster
+sim_data2 <- sim_data2[, 1:N_col]
+
+# Test on simulated data 
+outputCOCA_2 <- clusterofclusters(sim_data2)
+clusters2a <- outputCOCA_2$clusterLabels
+adjustedRandIndex(clusters1a, clusters2a)
 
