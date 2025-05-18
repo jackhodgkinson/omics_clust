4f940fe0fbe0ff4110a9eb3f5aef485c563b6d22
Jack Hodgkinson <jnhodgkinson2010@hotmail.co.uk>
2025-05-18 15:17:43 +0100

    Updates 18/05/2025

--- mds_threshold.R
+++ mds_threshold.R
@@ -20,7 +20,8 @@ set.seed(seed)
 N_col <- c(10, 100, 500, 1000)
 
 # Create empty results dataframe
-results <- data.frame(Columns = numeric(),
+results <- data.frame(Parameter.ID = numeric(),
+                      Columns = numeric(),
                       `True Number of Groups` = numeric(), 
                       Method = character(), 
                       Statistic = character(),
@@ -32,152 +33,204 @@ results <- data.frame(Columns = numeric(),
 # Parallel 
 parallel_process <- TRUE
 
-# Loop over different values of proteins
-for (i in N_col) {
+# Create many different parameters 
+params_list <- list(
+  # 1. Well-separated means, identity covariance
+  list(
+    cluster1 = list(mean = rnorm(i, mean = -3, sd = 0.1), cov = diag(i)),
+    cluster2 = list(mean = rnorm(i, mean = 0,  sd = 0.1), cov = diag(i)),
+    cluster3 = list(mean = rnorm(i, mean = 3,  sd = 0.1), cov = diag(i))
+  ),
   
-  # Less defined clusters with correlation
-  params <- list(
-    cluster1 = list(mean = rnorm(i, mean = -1, sd = 0.1), cov = cov(matrix(rnorm(i*i, mean = 0, sd = 0.5), nrow = i, ncol = i))),
-    cluster2 = list(mean = rnorm(i, mean = 0, sd = 0.1), cov = cov(matrix(rnorm(i*i, mean = 1, sd = 0.25), nrow = i, ncol = i))),
-    cluster3 = list(mean = rnorm(i, mean = 1, sd = 0.1), cov = cov(matrix(rnorm(i*i, mean = -1, sd = 0.15), nrow = i, ncol = i)))
+  # 2. Closer means, identity covariance
+  list(
+    cluster1 = list(mean = rnorm(i, mean = -1.5, sd = 0.1), cov = diag(i)),
+    cluster2 = list(mean = rnorm(i, mean = 0,    sd = 0.1), cov = diag(i)),
+    cluster3 = list(mean = rnorm(i, mean = 1.5,  sd = 0.1), cov = diag(i))
+  ),
+  
+  # 3. Closer still, diagonal covariance with varied scale
+  list(
+    cluster1 = list(mean = rnorm(i, mean = -1, sd = 0.1), cov = diag(runif(i, 0.5, 1.5))),
+    cluster2 = list(mean = rnorm(i, mean = 0,  sd = 0.1), cov = diag(runif(i, 0.5, 1.5))),
+    cluster3 = list(mean = rnorm(i, mean = 1,  sd = 0.1), cov = diag(runif(i, 0.5, 1.5)))
+  ),
+  
+  # 4. Full covariance, small overlap
+  list(
+    cluster1 = list(mean = rnorm(i, mean = -0.75, sd = 0.1), cov = cov(matrix(rnorm(i*i, 0, 0.5), ncol = i))),
+    cluster2 = list(mean = rnorm(i, mean = 0,     sd = 0.1), cov = cov(matrix(rnorm(i*i, 0, 0.5), ncol = i))),
+    cluster3 = list(mean = rnorm(i, mean = 0.75,  sd = 0.1), cov = cov(matrix(rnorm(i*i, 0, 0.5), ncol = i)))
+  ),
+  
+  # 5. High overlap, full covariance + correlation
+  list(
+    cluster1 = list(mean = rnorm(i, mean = -0.3, sd = 0.1), cov = cov(matrix(rnorm(i*i, 1, 0.3), ncol = i))),
+    cluster2 = list(mean = rnorm(i, mean = 0,    sd = 0.1), cov = cov(matrix(rnorm(i*i, 1, 0.3), ncol = i))),
+    cluster3 = list(mean = rnorm(i, mean = 0.3,  sd = 0.1), cov = cov(matrix(rnorm(i*i, 1, 0.3), ncol = i)))
   )
-  
-  # Simulate data
-  n_groups <- c(1, 2, 3, 4)
-  
-  # Get cores
-  n_cores <- numCores()
-  
-  for (n in n_groups) {
+)
+
+param_labels <- c(
+  "Well-separated means, identity cov",
+  "Closer means, identity cov",
+  "Closer still, diag cov varied scale",
+  "Full covariance, small overlap",
+  "High overlap, full covariance + correlation"
+)
+
+# Loop over different parameters
+for (param_index in seq_along(params_list)) {
+  param_set <- params_list[[param_index]] 
+  param_label <- param_labels[param_index]
+
+  # Loop over different values of proteins
+  for (i in N_col) {
     
-    data <- simulateGMM(3, n, params, n_indiv = 419, n_col = i,
-                         random_seed = seed,
-                         equal_clust = FALSE, equal_groups = FALSE)
-    true_clusters <- data[[2]]
+    # Simulate data
+    n_groups <- c(1, 2, 3, 4)
     
-    if (n > 1) {
-      true_groups <- data[[3]]
-    }
+    # Get cores
+    n_cores <- numCores()
     
-    data <- data[[1]]
-    
-    # Plot data
-    # data_plot <- as.matrix(data)
-    # annotationRow <- as.data.frame(true_clusters[1])
-    # names(annotationRow) <- "Clusters"
-    # 
-    # annotationCol <- as.data.frame(as.factor(true_groups))
-    # names(annotationCol) <- "ProteinClusters"
-    # rownames(annotationCol) <- colnames(data_plot)
-    # 
-    # rownames(data_plot) <- rownames(annotationRow)
-    # annotationRow$Clusters <- as.factor(annotationRow$Clusters)
-    # annotationCol$ProteinClusters <- as.factor(annotationCol$ProteinCluster)
-    # true_clust <- as.numeric(true_clusters[["group1_clusterid"]])
-    # ordered_data <- data_plot[order(true_clust), order(annotationCol$ProteinClusters)]
-    # pheatmap::pheatmap(
-    #   ordered_data,
-    #   annotation_row = annotationRow,
-    #   annotation_col = annotationCol[order(annotationCol$ProteinClusters), , drop = FALSE],
-    #   cluster_rows = FALSE
-    # )
-    
-    # Get distance matrix
-    classification <- GMMclassifier(data) # This does the MClust fitting - can replace with LCMM
-    
-    # Construct empty similarity matrix 
-    sim_matrix <- matrix(0, nrow = ncol(classification), ncol = ncol(classification),
-                         dimnames = list(colnames(classification), colnames(classification)))
-    
-    # Set seed 
-    set.seed(seed)
-    
-    if (parallel_process) {
+    for (n in n_groups) {
       
-      if (.Platform$OS.type == "windows") {
-        cl2 <- makeCluster(n_cores)
-        clusterExport(cl2, ls(envir = environment()), envir = environment())
+      data <- simulateGMM(3, n, params, n_indiv = 419, n_col = i,
+                           random_seed = seed,
+                           equal_clust = FALSE, equal_groups = FALSE)
+      true_clusters <- data[[2]]
+      
+      if (n > 1) {
+        true_groups <- data[[3]]
+      }
+      
+      data <- data[[1]]
+      
+      # Plot data
+      # data_plot <- as.matrix(data)
+      # annotationRow <- as.data.frame(true_clusters[1])
+      # names(annotationRow) <- "Clusters"
+      # 
+      # annotationCol <- as.data.frame(as.factor(true_groups))
+      # names(annotationCol) <- "ProteinClusters"
+      # rownames(annotationCol) <- colnames(data_plot)
+      # 
+      # rownames(data_plot) <- rownames(annotationRow)
+      # annotationRow$Clusters <- as.factor(annotationRow$Clusters)
+      # annotationCol$ProteinClusters <- as.factor(annotationCol$ProteinCluster)
+      # true_clust <- as.numeric(true_clusters[["group1_clusterid"]])
+      # ordered_data <- data_plot[order(true_clust), order(annotationCol$ProteinClusters)]
+      # pheatmap::pheatmap(
+      #   ordered_data,
+      #   annotation_row = annotationRow,
+      #   annotation_col = annotationCol[order(annotationCol$ProteinClusters), , drop = FALSE],
+      #   cluster_rows = FALSE
+      # )
+      
+      # Get distance matrix
+      classification <- GMMclassifier(data) # This does the MClust fitting - can replace with LCMM
+      
+      # Construct empty similarity matrix 
+      sim_matrix <- matrix(0, nrow = ncol(classification), ncol = ncol(classification),
+                           dimnames = list(colnames(classification), colnames(classification)))
+      
+      # Set seed 
+      set.seed(seed)
+      
+      if (parallel_process) {
         
-        # Parallelised computation of similarity matrix
-        sim_mat <- parLapply(cl2, 1:ncol(classification), function(i) {
+        if (.Platform$OS.type == "windows") {
+          cl2 <- makeCluster(n_cores)
+          clusterExport(cl2, ls(envir = environment()), envir = environment())
+          
+          # Parallelised computation of similarity matrix
+          sim_mat <- parLapply(cl2, 1:ncol(classification), function(i) {
+            sapply(1:ncol(classification), function(j) {
+              adjustedRandIndex(classification[[i]], classification[[j]])
+            })
+          })
+          
+          # Stop the parallel cluster after the work is done
+          stopCluster(cl2)
+          
+        } else {
+          sim_mat <- mclapply(1:ncol(classification), function(i) {
+            sapply(1:ncol(classification), function(j) {
+              adjustedRandIndex(classification[[i]], classification[[j]])
+            })
+          }, mc.cores = n_cores)
+        }
+      }
+      
+      else {
+        sim_mat <- lapply(1:ncol(classification), function(i) {
           sapply(1:ncol(classification), function(j) {
             adjustedRandIndex(classification[[i]], classification[[j]])
           })
         })
         
-        # Stop the parallel cluster after the work is done
-        stopCluster(cl2)
-        
-      } else {
-        sim_mat <- mclapply(1:ncol(classification), function(i) {
-          sapply(1:ncol(classification), function(j) {
-            adjustedRandIndex(classification[[i]], classification[[j]])
-          })
-        }, mc.cores = n_cores)
       }
-    }
-    
-    else {
-      sim_mat <- lapply(1:ncol(classification), function(i) {
-        sapply(1:ncol(classification), function(j) {
-          adjustedRandIndex(classification[[i]], classification[[j]])
-        })
-      })
       
-    }
-    
-    # Combine the list into a matrix
-    sim_matrix <- do.call(cbind, sim_mat)
-    
-    # Convert to dissimilarity matrix
-    dissim_matrix <- 1 - sim_matrix
-    dist_mat <- as.dist(dissim_matrix)
-    
-    # Perform MDS on the distance matrix
-    mds <- cmdscale(dist_mat)
-    
-    # Fit Gaussian and Gaussian Mixture (2 components) to MDS
-    models <- c("EII","VII","EEI","VEI","EVI","VVI","EEE",
-                "VEE","EVE","VVE","EEV","VEV","EVV","VVV")
-    for (m in models) {
-      start_time <- Sys.time()
-      test <- mclust::mclustBootstrapLRT(mds, modelName = m, nboot = 1000, level = 0.95, maxG = 1)
-      end_time <- Sys.time()
-      if (!is.null(test$p.value)) {
-        p <- round(test$p.value, 4)
-        results <- rbind(results, data.frame(
-          Columns = i,
-          `True Number of Groups` = n,
-          Method = paste("mclustBootstrapLRT:", m),
-          Statistic = paste("p-value:", p),
-          `Determined Number of Groups` = ifelse(p < 0.05, ">1", "1"),
-          RunTime = end_time - start_time
-        ))
-      } else {
-        # Optional: record failure
-        results <- rbind(results, data.frame(
-          Columns = i,
-          `True Number of Groups` = n,
-          Method = paste("mclustBootstrapLRT:", m),
-          Statistic = "p-value: NA",
-          `Determined Number of Groups` = "NA",
-          RunTime = "NA"
-        ))
+      # Combine the list into a matrix
+      sim_matrix <- do.call(cbind, sim_mat)
+      
+      # Convert to dissimilarity matrix
+      dissim_matrix <- 1 - sim_matrix
+      dist_mat <- as.dist(dissim_matrix)
+      
+      # Perform MDS on the distance matrix
+      mds <- cmdscale(dist_mat)
+      
+      # Fit Gaussian and Gaussian Mixture (2 components) to MDS
+      models <- c("EII","VII","EEI","VEI","EVI","VVI","EEE",
+                  "VEE","EVE","VVE","EEV","VEV","EVV","VVV")
+      for (m in models) {
+        start_time <- Sys.time()
+        test <- mclust::mclustBootstrapLRT(mds, modelName = m, nboot = 1000, level = 0.95, maxG = 1)
+        end_time <- Sys.time()
+        if (!is.null(test$p.value)) {
+          p <- round(test$p.value, 4)
+          results <- rbind(results, data.frame(
+            Parameter.ID = param_index,
+            Parameter.Label = param_label,
+            Columns = i,
+            `True Number of Groups` = n,
+            Method = paste("mclustBootstrapLRT:", m),
+            Statistic = paste("p-value:", p),
+            `Determined Number of Groups` = ifelse(p < 0.05, ">1", "1"),
+            RunTime = end_time - start_time
+          ))
+        } else {
+          # Optional: record failure
+          results <- rbind(results, data.frame(
+            Parameter.ID = param_index,
+            Parameter.Label = param_label,
+            Columns = i,
+            `True Number of Groups` = n,
+            Method = paste("mclustBootstrapLRT:", m),
+            Statistic = "p-value: NA",
+            `Determined Number of Groups` = "NA",
+            RunTime = "NA"
+          ))
+        }
       }
+      
+      # OTRIMLE
+      start_time = Sys.time()
+      model_otrimle <- otrimleg(mds, G=1:2)
+      end_time = Sys.time()
+      bic <- min(model_otrimle$ibic)
+      
+      results <- rbind(results, data.frame(Parameter.ID = param_index,
+                                           Parameter.Label = param_label,
+                                            Columns = i,
+                                           `True Number of Groups` = n,
+                                           Method = "OTRIMLE",
+                                           Statistic = paste("min(BIC):", bic),
+                                           `Determined Number of Groups` = ifelse(which.min(model_otrimle$ibic) == 1, "1", ">1"),
+                                           RunTime = end_time - start_time)
+      )
     }
-    
-    # OTRIMLE
-    start_time = Sys.time()
-    model_otrimle <- otrimleg(mds, G=1:2)
-    end_time = Sys.time()
-    bic <- min(model_otrimle$ibic)
-    
-    results <- rbind(results, data.frame(Columns = i,`True Number of Groups` = n,
-                                         Method = "OTRIMLE",
-                                         Statistic = paste("min(BIC):", bic),
-                                         `Determined Number of Groups` = ifelse(which.min(model_otrimle$ibic) == 1, "1", ">1"),
-                                         RunTime = end_time - start_time)
-    )
   }
 }
 
---
+++ threshold_analysis.R
@@ -0,0 +1,48 @@
+# MDS Threshold Results Analysis 
+# Load packages
+library(tidyverse)
+
+# Code
+mds_results <- read.csv("mds_results.csv")
+
+# Change data 
+mds_results <- mds_results %>%
+  mutate(TrueGroupLabel = ifelse(True.Number.of.Groups == 1, "1", ">1"),
+         DeterminedGroupLabel = Determined.Number.of.Groups) %>%
+  select(-True.Number.of.Groups, -Determined.Number.of.Groups)
+
+# Mclust success 
+mclust_success <- mds_results %>%
+  filter(str_detect(Method, "mclust")) %>%
+  mutate(Method =  str_replace(Method, "mclustBootstrapLRT: ", "")) 
+
+
+mclust_summary <- mclust_success %>%
+  mutate(Match = TrueGroupLabel == DeterminedGroupLabel) %>%
+  group_by(Method) %>%
+  summarise(Total = n(),
+    Matches = sum(Match, na.rm = TRUE),
+    MatchRate = round(Matches / Total * 100, 2))
+
+print(mclust_summary)
+
+mclust_success %>%
+  filter(Method %in% c("EEE","EEI")) %>%
+  group_by(Columns, Method) %>%
+  mutate(Match = TrueGroupLabel == DeterminedGroupLabel) %>%
+  summarise(Total = n(),
+            Matches = sum(Match, na.rm = TRUE),
+            MatchRate = round(Matches / Total * 100, 2))
+
+# OTRIMLE Success 
+otrimle_success <- mds_results %>%
+  filter(str_detect(Method, "OTRIMLE")) %>%
+  select(-Method)
+
+otrimle_summary <- otrimle_success %>%
+  mutate(Match = TrueGroupLabel == DeterminedGroupLabel) %>%
+  summarise(Total = n(),
+            Matches = sum(Match, na.rm = TRUE),
+            MatchRate = round(Matches / Total * 100, 2))
+
+print(otrimle_summary)
