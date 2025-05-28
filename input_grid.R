ccd48f08916868ccb7e11734ec836debe381bba4
Jack Hodgkinson <jnhodgkinson2010@hotmail.co.uk>
2025-05-28 16:52:57 +0100

    Updates 28/05/2025

--- input_grid.R
+++ input_grid.R
@@ -19,7 +19,9 @@ grid$task_id <- seq_len(nrow(grid))
 
 # Subset failed ones 
 grid <- grid %>%
-  filter(task_id %in% c(143, 1038, 998, 1018, 978))
+  filter(task_id %in% c(25, 105, 143, 185, 269, 305, 325, 445, 485, 505, 529, 
+                        549, 586, 605, 618, 645, 665, 805, 829, 845, 849, 897, 
+                        909, 925, 949, 969, 978, 998, 1005, 1018, 1038, 1089, 1109))
 
 # Write to CSV
 write.table(grid, "input_grid.csv", sep = ",", quote = FALSE, row.names = FALSE, col.names = FALSE)
